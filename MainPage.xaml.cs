71db9dc765ebecc4d95a38bb836fe848a6a9c720
JollyRodgerNAS <nick.stewart1299@gmail.com>
2024-12-21 23:47:48 -0600

    test commit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,9 +12,10 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
+		int test = 3;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			CounterBtn.Text = $"Clicked {count} time {test}";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
 
